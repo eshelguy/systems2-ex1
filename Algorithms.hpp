1809458a12fa74d6ac493438f58ca779a6c6b54e
GuyEshel <134394173+eshelguy@users.noreply.github.com>
2024-08-08 09:42:05 +0300

    Update Algorithms.hpp

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -1,5 +1,4 @@
-//guyes134@gmail.com
-
+//mail: guyes134@gmail.com
 
 #pragma once
 #include "Graph.hpp"
@@ -12,4 +11,9 @@ namespace Algorithms{
     std::string isBipartite(const Graph &g);
     std::string negativeCycle(const Graph &g);
     void DFS(Graph g, int v, std::vector<bool> &visited);
-}
+    std::string BFSShortestPath(const Graph &g, int start, int end);
+    std::string DijkstraShortestPath(const Graph &g, int start, int end);
+    std::string BellmanFordShortestPath(const Graph &g, int start, int end);
+    bool BFSUtil(const Graph &g, int src, std::vector<int> &colors, std::vector<int> &setA, std::vector<int> &setB);
+    bool DFSUtil(const Graph &g, int v, std::vector<bool> &visited, std::vector<int> &parent, int &cycleStart, int &cycleEnd);
+    }
