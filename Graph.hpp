7ba997a0da35924082218bdd5d4c11d08f281466
GuyEshel <134394173+eshelguy@users.noreply.github.com>
2024-08-09 13:34:25 +0300

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -23,9 +23,11 @@ public:
     bool hasNegativeWeights() const;
     bool isDirectedGraph() const;
 
+
     //constructors
     Graph();
     Graph(std::vector<std::vector<int>> matrix, bool direction, bool negWeight, bool weight, int veNum, int edgNum);
+
     ~Graph();
 
     int getVertexNum() const{
@@ -52,3 +54,33 @@ public:
     }
 
     bool isTheSameSize(const Graph& graph) const;
+
+    bool isWeightedGraph() const;
+
+    //Arithmetic operators
+    Graph operator+() const; //unary
+    Graph operator+(const Graph& graph) const; //binary
+    Graph& operator+=(const Graph& graph);
+    Graph operator-() const; //unary
+    Graph operator-(const Graph& graph) const; //binary
+    Graph& operator-=(const Graph& graph);
+
+    //Compare operators
+    bool operator>(const Graph& graph);
+    bool operator>=(const Graph& graph);
+    bool operator<(const Graph& graph);
+    bool operator<=(const Graph& graph);
+    bool operator==(const Graph& graph);
+    bool operator!=(const Graph& graph);
+
+    //prefix and postfix operators
+    Graph& operator++();
+    Graph operator++(int);
+    Graph& operator--();
+    Graph operator--(int);
+
+    //Multiplication operators
+    Graph& operator*(int num);
+    Graph operator*(const Graph& graph) const;
+    friend std::ostream &operator<<(std::ostream &output, const Graph &g);
+};
